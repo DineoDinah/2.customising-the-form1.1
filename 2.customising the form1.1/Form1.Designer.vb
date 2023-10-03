faa420a5b68588080d54025f8b0c06e5ce199a18
Lenovo-User <Lenovo-User@DESKTOP-RNPFAS5>
2023-10-03 10:49:42 +0200

    Add project files.

---
+++ 2.customising the form1.1/Form1.Designer.vb
@@ -0,0 +1,61 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+	Inherits System.Windows.Forms.Form
+
+	'Form overrides dispose to clean up the component list.
+	<System.Diagnostics.DebuggerNonUserCode()>
+	Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+		Try
+			If disposing AndAlso components IsNot Nothing Then
+				components.Dispose()
+			End If
+		Finally
+			MyBase.Dispose(disposing)
+		End Try
+	End Sub
+
+	'Required by the Windows Form Designer
+	Private components As System.ComponentModel.IContainer
+
+	'NOTE: The following procedure is required by the Windows Form Designer
+	'It can be modified using the Windows Form Designer.  
+	'Do not modify it using the code editor.
+	<System.Diagnostics.DebuggerStepThrough()>
+	Private Sub InitializeComponent()
+		btnClose = New Button()
+		Btn = New Button()
+		SuspendLayout()
+		' 
+		' btnClose
+		' 
+		btnClose.Location = New Point(372, 322)
+		btnClose.Name = "btnClose"
+		btnClose.Size = New Size(112, 34)
+		btnClose.TabIndex = 0
+		btnClose.Text = "Close"
+		btnClose.UseVisualStyleBackColor = True
+		' 
+		' Btn
+		' 
+		Btn.Location = New Point(208, 177)
+		Btn.Name = "Btn"
+		Btn.Size = New Size(359, 81)
+		Btn.TabIndex = 2
+		Btn.Text = "This is Dineo"
+		Btn.UseVisualStyleBackColor = True
+		' 
+		' Form1
+		' 
+		AutoScaleDimensions = New SizeF(10F, 25F)
+		AutoScaleMode = AutoScaleMode.Font
+		ClientSize = New Size(800, 450)
+		Controls.Add(Btn)
+		Controls.Add(btnClose)
+		Name = "Form1"
+		Text = "Close"
+		ResumeLayout(False)
+	End Sub
+
+	Friend WithEvents btnClose As Button
+	Friend WithEvents Btn As Button
+End Class
---
+++ 2.customising the form1.1/Form1.vb
@@ -0,0 +1,29 @@
+﻿Public Class Form1
+	Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+		Me.Text = "My first App in 2023"
+		Me.BackColor = Color.Red
+		Me.MaximizeBox = True
+		Me.MinimizeBox = True
+		Me.Size = New Size(800, 400)
+		Me.Opacity = 0.3  'makes it a bit transport
+		Me.CenterToParent() 'centers the execution box
+
+	End Sub
+
+	Private Sub BtnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+		Close()
+	End Sub
+
+
+
+	Private Sub Btn_Click(sender As Object, e As EventArgs) Handles Btn.Click
+
+
+		MsgBox("This is Dineo",, "Greeting") ' this is to make the lil box have Hello instead of default words
+
+
+	End Sub
+End Class
+
+
